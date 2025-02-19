9f8bf8eb4d5748107b965a73e459a8de81131025
ejlzhj <127798716+ejlzhj@users.noreply.github.com>
2025-02-18 20:36:44 -0500

    Added UNTESTED two-part Coral intake command
    Commands added: Priming coral and releasing coral

--- intake.py
+++ intake.py
@@ -1,6 +1,6 @@
 import rev
 import wpilib
-from wpilib import TimedRobot, Joystick
+from wpilib import TimedRobot, Joystick, DigitalInput
 import commands2
 from commands2 import Command
 from commands2 import Subsystem
@@ -12,6 +12,9 @@ class IntakeSubsystem(Subsystem):
         #0 & 1 are placeholder numbers
         self.leftMotor = rev.SparkMax(12, rev.SparkMax.MotorType.kBrushless)
         self.rightMotor = rev.SparkMax(11, rev.SparkMax.MotorType.kBrushless)#inverted
+        self.placeholderNumber = 0 #Replace with actual number
+        self.coralSensor = DigitalInput(self.placeHoldernumber)
+        #conveyorDetector.whileActiveContinuous(new RunMotor());
         self.motors = wpilib.MotorControllerGroup(self.leftMotor, self.rightMotor)
 
     def intake(self):
@@ -22,6 +25,27 @@ class IntakeSubsystem(Subsystem):
     def stop(self):
         self.leftMotor.set(0.0)
         self.rightMotor.set(0.0)
+    
+    def getCoralSensorInput(self):
+        self.coralSensor.get()#true when & false when?
+    
+    def primeCoral(self):
+        #coral goes in
+        #sensor senses coral
+        if self.coralSensor.get():
+            self.leftMotor.set(0.8)
+            self.rightMotor.set(-0.8)
+        while self.coralSensor.get():
+            pass
+        else:#sensor stops sensing coral
+            self.leftMotor.set(0.0)
+            self.rightMotor.set(0.0)
+        #motors stop
+    
+    def releaseCoral(self):
+        #button press and go
+        self.leftMotor.set(0.8)
+        self.rightMotor.set(-0.8)
 
 
 class IntakeCommand(Command):
@@ -35,6 +59,36 @@ class IntakeCommand(Command):
 
     def execute(self):
         self.intake_subsystem.intake()
+
+    def end(self, interrupted):
+        self.intake_subsystem.stop()
+
+class PrimeCoralCommand(Command):
+    def __init__(self, intake_subsystem):
+        super().__init__()
+
+        self.intake_subsystem = intake_subsystem
+
+    def initialize(self):
+        pass
+
+    def execute(self):
+        self.intake_subsystem.primeCoral()
+
+    def end(self, interrupted):
+        self.intake_subsystem.stop()
+
+class ReleaseCoralCommand(Command):
+    def __init__(self, intake_subsystem):
+        super().__init__()
+
+        self.intake_subsystem = intake_subsystem
+
+    def initialize(self):
+        pass
+
+    def execute(self):
+        self.intake_subsystem.releaseCoral()
 
     def end(self, interrupted):
         self.intake_subsystem.stop()
